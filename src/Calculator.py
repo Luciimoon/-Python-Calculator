ec87d42189b801141c9731d25613f539d0eb7449
compsci <mitchell_wallace@ymail.com>
2018-12-14 10:33:18 -0700

    chagnes raw_input to input, removed parentheses

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,5 @@
-
 Running = True
+
 
 def simplify(num, msg):
     del msg[0:3]
@@ -8,47 +8,48 @@ def simplify(num, msg):
 
 def calculate(msg):
     msg = [x for x in msg if x != ' ']
-    while (len(msg) > 1):
+    while len(msg) > 1:
 
-        if(msg[0].isdigit() and msg[1].isdigit()):
+        if msg[0].isdigit() and msg[1].isdigit():
             msg[:2] = [''.join(msg[:2])]
-        if(len(msg) > 3):
-            if(msg[2].isdigit() and msg[3].isdigit()):
+        if len(msg) > 3:
+            if msg[2].isdigit() and msg[3].isdigit():
                 msg[2:4] = [''.join(msg[2:4])]
 
-        if ('+' in msg[:2]):
+        if '+' in msg[:2]:
             cal = float(msg[0]) + float(msg[2])
             simplify(cal, msg)
-        elif ('-' in msg[:2]):
+        elif '-' in msg[:2]:
             cal = float(msg[0]) - float(msg[2])
             simplify(cal, msg)
-        elif ('*' in msg[:2]):
+        elif '*' in msg[:2]:
             cal = float(msg[0]) * float(msg[2])
             simplify(cal, msg)
-        elif ('/' in msg[:2]):
+        elif '/' in msg[:2]:
             cal = float(msg[0]) / float(msg[2])
             simplify(cal, msg)
-        elif ('**' in msg[:3] or '^' in msg[:2]):
+        elif '**' in msg[:3] or '^' in msg[:2]:
             cal = float(msg[0]) ** float(msg[2])
             simplify(cal, msg)
-        elif ('//' in msg[:3]):
+        elif '//' in msg[:3]:
             cal = float(msg[0]) // float(msg[2])
             simplify(cal, msg)
-        elif ('%' in msg[:2]):
+        elif '%' in msg[:2]:
             cal = float(msg[0]) % float(msg[2])
             simplify(cal, msg)
         else:
             cal = 'error'
     print(cal)
 
+
 while (Running):
     x = list(input('equation?'))
     calculate(x)
 
     GoAgain = input("\ndo you want to go again?").lower()
-    if (GoAgain == "yes" or GoAgain == "y"):
+    if GoAgain == "yes" or GoAgain == "y":
         Running = True
-    elif (GoAgain == "no" or GoAgain == "n"):
+    elif GoAgain == "no" or GoAgain == "n":
         Running = False
     else:
         print('error, please try again')
